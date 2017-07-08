00a1cd742c34fc0fa3a44add5b2f654ca6b3b774
Oleg Litvin <olitvin@zuzex.com>
2017-07-09 02:43:16 +0300

    new version

---
+++ src/directives/ng-flags.js
@@ -0,0 +1,39 @@
+/**
+ * ng-flags module
+ * Turns country ISO code to flag thumbnail.
+ *
+ * Author: asafdav - https://github.com/asafdav
+ */
+angular.module('ngFlag', []).
+directive('flag', function() {
+    return {
+        restrict: 'E',
+        replace: true,
+        template: '<span><span class="flag {{::country}}"></span></span>',
+        scope: {
+            country: '=',
+            size: '='
+        },
+        link: function(scope, elm, attrs) {
+            // Default flag size
+            if (!scope.size) {
+                scope.size = 16;
+            }
+
+            var $unwatchC = scope.$watch('country', function(value) {
+                scope.country = angular.lowercase(value);
+                if (value) {
+                    $unwatchC();
+                }
+            });
+
+            var $unwatchS = scope.$watch('size', function(value) {
+                scope.size = value;
+                if (value) {
+                    $unwatchS();
+                }
+                elm.setClass('f' + scope.size);
+            });
+        }
+    };
+});
